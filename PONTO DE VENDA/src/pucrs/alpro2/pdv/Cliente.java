a61137d1d37861ee72e087bec8c3d565ac48ebc0
iann <iann@GLADIS>
2014-04-29 20:58:33 -0300

    classes iniciais

---
+++ PONTO DE VENDA/src/pucrs/alpro2/pdv/Cliente.java
@@ -0,0 +1,53 @@
+package pucrs.alpro2.pdv;
+
+/**
+ * A classe <code>Cliente</code> representa um cliente de supermercado.
+ * 
+ * <p>
+ * Um objeto da classe cliente armazena o número de cadastro de pessoa física
+ * para fins de nota fiscal.
+ * 
+ * <p>
+ * O objeto armazena um endereço de correio eletrônico para envio de cópia de
+ * nota fiscal e para relacionamento com o cliente.
+ * 
+ * <p>
+ * O número de cadastro de pessoa física deve ser verificado para evitar erros
+ * de digitação e cadastro duplicado.
+ * 
+ * <p>
+ * Caso o cliente não informe ou não tenho um número de CPF, indicar a constante
+ * CPF_INDEFINIDO.
+ * 
+ * @see http://www.receita.fazenda.gov.br/pessoafisica/cpf/cadastropf.htm
+ * @see http://pt.wikipedia.org/wiki/Cpf
+ * @see https://nfg.sefaz.rs.gov.br
+ * 
+ * @author marco.mangan@pucrs.br
+ */
+public class Cliente {
+	public final static String CPF_INDEFINIDO = "CPF_INDEFINIDO";
+
+	private String cpf;
+	private String email;
+
+	public Cliente(final String cpf, final String email) {
+		super();
+		this.cpf = cpf;
+		this.email = email;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("Cliente [cpf=%s, email=%s]", cpf, email);
+	}
+
+}
